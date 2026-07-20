e1558ef2e2ed980696677676cb569bbfe8446e4e
pankaj kumar <pk1912pankaj.gmail.com>
2026-07-20 22:43:29 +0530

    this is my first commit

--- src/ADOGenerator/Models/AccessDetails.cs
+++ src/ADOGenerator/Models/AccessDetails.cs
@@ -2,6 +2,8 @@
 {
     public class AccessDetails
     {
+
+        #my first changes 
         public string access_token { get; set; }
         public string token_type { get; set; }
         public string expires_in { get; set; }
